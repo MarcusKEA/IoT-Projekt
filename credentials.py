29b5094dd3ae29d9611d3f566f110fdc1df35c24
BuildTools <unconfigured@null.spigotmc.org>
2021-11-09 19:45:20 +0100

    First Push

---
+++ credentials.py
@@ -0,0 +1,10 @@
+credentials = {
+    'ssid' : 'OnePlu',
+    'password' : '09876543',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'Xeus',
+    'ADAFRUIT_IO_KEY' : b'aio_GzlY34gzfXNFw7Yx2qOcbq1ACuj9',
+    'ADAFRUIT_IO_PUB_FEEDNAME' : b'bot_pub',
+    'ADAFRUIT_IO_SUB_FEEDNAME' : b'bot_sub'
+    }
+
